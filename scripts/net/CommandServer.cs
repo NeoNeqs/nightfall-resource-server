b70cfbf9b6ca2e58d9a963b177f91fe40d1f039c
NeqsTech <theneqstech@gmail.com>
2021-09-28 00:19:43 +0200

    Initial commit

---
+++ scripts/net/CommandServer.cs
@@ -0,0 +1,24 @@
+using Godot;
+using Nightfall.ServerUtils.Net;
+using Nightfall.SharedUtils.InfoCodes;
+using Nightfall.SharedUtils.Net.Messaging;
+
+namespace Nightfall.Net
+{
+    public partial class CommandServer : Node // TODO: Rename CommandServer to sth else. The name doesn't really fit it's purpose.
+    {
+        private readonly TCPMessageServer _tcpMessageServer = new();
+
+        public override void _Ready()
+        {
+            _tcpMessageServer.MessageReceivedEvent += OnMessageReceived;
+
+            _tcpMessageServer.Listen(6901);
+        }
+
+        private static void OnMessageReceived(NFGuid nfGuid, Message message, long error)
+        {
+            if (error != NFError.Ok) return;
+        }
+    }
+}
